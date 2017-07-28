91b5b6f2714d501da3a9f741075c327ca2d44b54
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-07-28 09:49:14 +0200

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -13,27 +13,34 @@ expressApp.use(bodyParser.urlencoded({
 }));
 expressApp.use(bodyParser.json());
 // expressApp.post('/PATH', function (request, response) {
-expressApp.post('/', function (req, res) {
- 
+expressApp.post('/contexts', function (req, res) {
+    var theData = req.body.result.resolvedQuery;
+    var theSpeech = "I'm so geat !!"
+    return res.json({
+        speech: theData,
+        displayText: theSpeech,
+        source: 'First_ChatBot',
+        data: theData
+    });
+
+    var request = app.textRequest('', {
+        sessionId: '4f330a33-a3a9-4167-b2b5-d04eaa86d8b5'
+    });
 
 
 
- var request = app.textRequest('', {
-     sessionId: '4f330a33-a3a9-4167-b2b5-d04eaa86d8b5'
- });
 
+    request.on('response', function(response) {
+        console.log(response);
+        res.send(response);
+    });
 
+    request.on('error', function(error) {
+        console.log(error);
+        res.send(error); 
+    });
 
-
- request.on('response', function(response) {
-     console.log(response);
- });
-
- request.on('error', function(error) {
-     console.log(error);
- });
-
- request.end();
+    request.end();
 
 });
  
@@ -41,15 +48,3 @@ var server = expressApp.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port);
 });
-
-
-
-
-
-
-
-
-
-
-
-
