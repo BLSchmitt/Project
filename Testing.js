2ead8554345e7fac5808933a21db028e6d06db25
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-08 11:19:43 +0200

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -70,16 +70,7 @@ expressApp.post('/', function (req, res) {
 //*
     var request = app.textRequest('', {
         sessionId: 'e5c6eb93-3255-4640-b48f-bfb04298b74e'
-	contexts: 
-	[
-		{
-			name: "naming_test_dialog_context",
-			parameters: {
-				"krom": theNameS,
-				"given-name": theNameS
-			}
-        }
-    ]
+	 
     });
 
 
