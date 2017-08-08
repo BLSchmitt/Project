305f30eda3f99afaaf55c61ad0c46b9de59cb4de
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-08 11:56:04 +0200

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -39,8 +39,6 @@ expressApp.post('/', function (req, res) {
 		"displayText": "do we care about this one ?",
 		"source": 'First_ChatBot',
 		"data": theData,
-		"given-name": theNameS ,
-
 			
 		/*
 		// that how we call an event.
