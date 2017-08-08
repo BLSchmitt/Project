92e1fdf48b6b50f6cc6bd142639bc2a0c20a431b
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-08 10:16:00 +0200

    Update Testing.js

--- Testing.js
+++ Testing.js
@@ -63,14 +63,9 @@ expressApp.post('/', function (req, res) {
 			"displayText": "do we care about this one ?",
 			"source": 'First_ChatBot',
 			"data": theData,
-			 contexts: [
-			{
-				name: "naming_test_dialog_context",
-				parameters: {
-					"given-name": theNameS
-				}
+			parameters: {
+				"given-name": theNameS
 			}
-			]
 	   });
    };
 	
