83859e9bbabe2643dbe7d62821ba40e1544c3546
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-10 15:46:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var apiai = require('apiai');
 
 var app = apiai("199651c380d041f3bca8829faf829f1c");
@@ -10,15 +12,28 @@ expressApp.use(bodyParser.urlencoded({
     extended: true
 }));
 expressApp.use(bodyParser.json());
-expressApp.post('/echo', function (req, res) {
+expressApp.post('/', function (req, res) {
     var data = req.body.result.resolvedQuery;
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+   
+    
     return res.json({
         speech: data,
         displayText: data,
-        source: 'webhook-echo-sample',
+        source: 'test_2_chatbot',
         data: data
     });
+    
+    return res.json({
+		"followupEvent": {
+				 "name": "event_OTD_test",
+				 // that doesn't work i don't know why now 08/08/2017
+				 "data": {
+						 "truc": theSpeech
+				  }
+		}
+	});
+    
     
     var request = app.textRequest('', {
         sessionId: 'e5c6eb93-3255-4640-b48f-bfb04298b74e'
@@ -38,6 +53,7 @@ expressApp.post('/echo', function (req, res) {
 })
 
 var server = expressApp.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("Express is working on port " + port);
+    var port = server.address().port;
+    console.log("Express is working on port " + port);
+    console.log("Fin du programme ");
 });
