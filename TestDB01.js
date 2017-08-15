08dfaf252e98a4516ff86fbebfd76df72637d0ee
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-15 16:48:08 +0200

    Update TestDB01.js

--- TestDB01.js
+++ TestDB01.js
@@ -49,9 +49,63 @@ expressApp.post('/', function (req, res) {
 
 				if(theNameS != "\"\"" && theKromS != "\"\""){
 					console.log("MWHAHAHAHHAAHAAHHA, too easy, way too easy ...");
+					
+					var mySQLString = "START TRANSACTION;";
+					
+					var db_config = {
+						host: 'eu-cdbr-west-01.cleardb.com',
+						user: 'ba575c324edc45',
+						password: 'e155580c',
+						database: 'heroku_04b4aac313a5863'
+					};
+
+					var connection;
+					
+mySQLString += 'INSERT INTO case_files (name,krom) VALUES ( "' + theNameS +  '","' + theKromS +  '");';
+
+					function handleDisconnect() {
+						console.log('1. connecting to db:');
+						connection = mysql.createConnection(db_config); // Recreate the connection, since
+																		// the old one cannot be reused.
+
+						connection.connect(function(err) {              	// The server is either down
+							if (err) {                                     // or restarting (takes a while sometimes).
+								console.log('2. error when connecting to db:', err);
+								setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+							}                                     	// to avoid a hot loop, and to allow our node script to
+						});                                     	// process asynchronous requests in the meantime.
+																	// If you're also serving http, display a 503 error.
+						connection.on('error', function(err) {
+							console.log('3. db error', err);
+							if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+								handleDisconnect();                      	// lost due to either server restart, or a
+							} else {                                      	// connnection idle timeout (the wait_timeout
+								throw err;                                  // server variable configures this)
+							}
+						});
+					}
+
+					handleDisconnect();
+					
+					putInSQL();
+
+				   function putInSQL() {
+							mySQLString += "COMMIT;"
+							
+					   		connection.query(mySQLString, function(err, rows, fields) {
+							if (err) {
+								console.log('error: ', err);
+								throw err;
+							}
+							
+							mySQLString = "";
+							mySQLString = "START TRANSACTION;"
+						});
+				   }
+					
 
 	// start here to go to mysql
-				//*
+				/*
 					//var mysql = require('mysql'); already written on top
 					var mySQLString = "START TRANSACTION;";
 
@@ -93,14 +147,14 @@ mySQLString += 'INSERT INTO case_files (description,name,email,contact_number,sy
 	}
 
 
-return res.json({
-			"speech": theSpeech,
-			"displayText": "do we care about this one ?",
-			"source": 'First_ChatBot',
-			"data": theData,
-	   });
+	return res.json({
+		"speech": theSpeech,
+		"displayText": "do we care about this one ?",
+		"source": 'First_ChatBot',
+		"data": theData,
+	});
 
-var request = app.textRequest('', {
+	var request = app.textRequest('', {
 		sessionId: '94977757-7fc6-4c81-936a-b1fcfaaf9a7f'
 	});
 
@@ -126,16 +180,6 @@ var server = expressApp.listen(process.env.PORT || 5000, function () {
 	console.log("Fin du programme ");
 
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
