6a548d8f04a7b43c1cb0f93c5f730013df22a0e7
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-16 16:16:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ expressApp.use(bodyParser.urlencoded({
 expressApp.use(bodyParser.json());
 expressApp.post('/', function (req, res) {
  
+	
 	// if action == find_information_1 then send a context that record the info 
 	// with another name than the name in record_context. to make sure information_2 don't erase the data
+	// send a context with name and email remplace by the value of name_2 and email_2
 	
 		// **TO DO**
 
 	// end if
 	
     
+	// In order to check the system ID we need to trigger it.
 	// if the action is save_case_type_data or ask_system_ID then call event event_find_system_ID
 		/*
 		 return res.json({
@@ -36,6 +39,9 @@ expressApp.post('/', function (req, res) {
 		//*/
 	// end if
 	
+	// we need to make sur we have all the data before the laste confirmation.
+	// so we need to check in the record_context if we have everything (name or given_name or name_2 && problem_desc.original && status ... )
+	// when we have all the data we need, we juste have to call the final checking.
 	// if action is save_location_data then call event_final_confirmation
 		/*
 		 return res.json({
@@ -50,12 +56,27 @@ expressApp.post('/', function (req, res) {
 		//*/
 	// end if
 	
+	// if there is a data that is wrong in the last confirmation we need first to find the one we want to change.
+	// then we change it then we need to retrigger the big_followup
 	// if action == Big_followup.Big_followup-no
 		// switch
 		// case  body.result.resolvedQuery == "name"
 			// then call event_final_correction_name
 		// case body.result.resolvedQuery == "email"
 		// case ...
+	
+	
+		// then blabla 
+		// then 
+		/*
+		"followupEvent": {
+			"name": "event_find_system_ID",
+			// that doesn't work i don't know why now 08/08/2017
+			//	 "data": {
+			//			 "truc": theSpeech
+			//	  }
+		}
+		//*/
 	// end if
 	
     
