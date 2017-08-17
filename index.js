dcef6fe4a4ea374af981f8aa0b11d16aa3b5181e
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-17 14:34:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ expressApp.post('/', function (req, res) {
 	// system_ID
 	// In order to check the system ID we need to trigger it.
 	// if the action is save_case_type_data or ask_system_ID then call event event_find_system_ID
-	if (req.body.action == "save_case_type_data" || req.body.action == "ask_system_ID"){
+	if (req.body.result.action == "save_case_type_data" || req.body.result.action == "ask_system_ID"){
 		console.log("system_id event working");
    		return res.json({
 			"followupEvent": {
