f338e033f27ce669bc7b3cc611d14acddf156549
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-17 11:42:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,11 +31,11 @@ expressApp.post('/', function (req, res) {
 		console.log(""+req.body.result.contexts[i].parameters.name)
 		var recordContext = req.body.result.contexts[i].parameters;
 		
-		if( recordContext.name == undefined || recordContext.name == "\"\"" ){
+		if( (recordContext.name == undefined || recordContext.name == "\"\"") && (recordContext.given-name == undefined || recordContext.given-name == "\"\"") && (recordContext.name_2 == undefined || recordContext.name_2 == "\"\"") && (recordContext.given-name_2 == undefined || recordContext.given-name_2 == "\"\"") ){
 			console.log("ok it works carry on");
 		}
 /*		
-&& recordContext.given-name == undefined && recordContext.name_2 == undefined && recordContext.given-name_2 == undefined
+
 		else{
 			if( recordContext.name == "\"\"" &&  recordContext.given-name == "\"\"" && recordContext.name_2 == "\"\"" && recordContext.given-name_2 == "\"\"" ){
 				
