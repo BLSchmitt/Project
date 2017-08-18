a259a455f1e9fbeedfec5d47d7826b3a0247e44e
BLSchmitt <30492877+BLSchmitt@users.noreply.github.com>
2017-08-18 10:16:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,13 +88,23 @@ expressApp.post('/', function (req, res) {
 						else{
 							if( recordContext.case_type == "" || recordContext.case_type == undefined ){
 							// Case_type
-								// problem_desc_no_fallback
-								return res.json({
-									"speech": "Please give me the case type. We have 4 different case type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation). \nWhich one is yours ? \nYou can use a sentence as : \nThe case type is TheCaseType",
-									"displayText": "Please give me the case type. We have 4 different case type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation). \nWhich one is yours ? \nYou can use a sentence as : \nThe case type is TheCaseType",
-									"source": 'test_2_cahtbot',
-									"data": ""
-							   });
+								if(req.body.result.metadata.intentName == "problem_desc_no_fallback"){
+									return res.json({
+										"speech": "You need to choose one of this 4 cate type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation).",
+										"displayText": "You need to choose one of this 4 cate type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation).",
+										"source": 'test_2_cahtbot',
+										"data": ""
+								   });
+								}
+								else{
+									// problem_desc_no_fallback
+									return res.json({
+										"speech": "Please give me the case type. We have 4 different case type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation). \nWhich one is yours ? \nYou can use a sentence as : \nThe case type is TheCaseType",
+										"displayText": "Please give me the case type. We have 4 different case type : \nC-job (problem, corrective maintenance, issue), \nP-job (service, planned maintenance), \nK-job (upgrade, update), \nI-job (Installation). \nWhich one is yours ? \nYou can use a sentence as : \nThe case type is TheCaseType",
+										"source": 'test_2_cahtbot',
+										"data": ""
+								   });
+								}
 							}
 							else{
 								if( recordContext.system_id == "" || recordContext.system_id == undefined ){
